bf0c10c1e23ab31503b152a815089171532c9785
discpic-ron <granddriod1210@gmail.com>
2026-03-21 21:37:31 -0400

    Add Interpreter class with initial methods

---
+++ source/interpreter.py
@@ -0,0 +1,12 @@
+class Interpreter:
+  def __init__(self):
+    self.variables = {}
+
+  def get_kind(self,token,kind): #this is a helper in converting token to value based on kind
+    pass
+
+  def evaluate_expression(self,node):
+    pass
+
+  def execute(self):
+    pass
