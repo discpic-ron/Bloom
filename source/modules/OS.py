f12fb2b748f45cbef5238d4e627728d083e91d5b
discpic-ron <granddriod1210@gmail.com>
2026-03-22 14:17:33 -0400

    Refactor fs class to integrate file_system

---
+++ source/modules/OS.py
@@ -0,0 +1,23 @@
+from File_system import file_system
+
+class fs:
+  def __init__(self):
+    self.files = file_system().files
+    
+  def write(self,path):
+    pass
+  
+  def read(self,path):
+    pass
+  
+  def list(self,directory):
+    pass
+  
+  def exists(self,path):
+    pass
+    
+  def is_dir(self,path):
+    pass
+  
+  def mkdir(self,path):
+    pass
