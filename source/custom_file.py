42f2a30f7223a0a4d968cf772129006397277545
discpic-ron <granddriod1210@gmail.com>
2026-03-21 21:25:20 -0400

    Add custom file creation and reading functions

---
+++ source/custom_file.py
@@ -0,0 +1,17 @@
+custom_extension = ".bl"
+file_name = "" + custom_extension
+def create_custom_file(data):
+  try:
+    with open(file_name, 'w') as custom_file:
+        custom_file.write(data)
+    print(f"File '{file_name}' created successfully with custom format.")
+  except IOError as e:
+      print(f"An error occurred: {e}")
+
+def read_custom_file():
+  try:
+      with open(filename, 'r') as custom_file:
+          content = custom_file.read()
+      print(f"\nContents of '{file_name}':\n{content}")
+  except FileNotFoundError:
+      print(f"File '{file_name}' not found.")
