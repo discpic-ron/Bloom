d52331cdb1a6a213a96385578fc7f9ae91c9a0b6
discpic-ron <granddriod1210@gmail.com>
2026-03-21 21:32:43 -0400

    Add initial parser class with methods

---
+++ source/parser.py
@@ -0,0 +1,12 @@
+class parse:
+  def __init__(self,tokens):
+    self.tokens = tokens
+
+  def search(self):
+    pass
+
+  def parse_assignment(self):
+    logical = False
+
+  def give_info(self):
+    pass
